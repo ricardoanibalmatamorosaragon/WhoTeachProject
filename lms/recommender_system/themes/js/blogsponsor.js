0a0b9911cf86d3c89c5f29e42ff4613b2d44d615
StefanoF96 <63451717+StefanoF96@users.noreply.github.com>
2020-04-20 15:18:42 +0200

    connessione sempre al db standard(recommender_sys)
    Rimosso davanti alle query il prefisso Sql973959_3.  In modo che la connessione venga effettuata sempre al nuovo db standard(in caso il db venga cambiato in futuro).  Il db di connessione standard si può recuperare dai file /config.php, /lib/mysql_conn.php,
      /search_engine/php/mysql_conn.php

---
+++ lms/recommender_system/themes/js/blogsponsor.js
@@ -0,0 +1,2 @@
+document.write('<script type="text/javascript" src="http://cdn.adpacks.com/adpacks.js?legacyid=1258449&zoneid=1386&key=ef0b652d262a926ea219420f9f2cf058&serve=C6SI42Y&placement=cssglobecom&circle=dev" id="_adpacks_js"></script>');
+document.write('<style>.bsap{float:left;position:fixed;z-index:1000;top:20px;right:20px;width:300px;overflow:hidden;}.bsa_it_ad{overflow:hidden;}</style>');
