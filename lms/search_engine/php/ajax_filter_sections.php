ab524153f6b5418d4c0b259a95d5039bcb6c3847
StefanoF96 <63451717+StefanoF96@users.noreply.github.com>
2020-04-21 14:34:33 +0200

    fix search_engine

--- lms/search_engine/php/ajax_filter_sections.php
+++ lms/search_engine/php/ajax_filter_sections.php
@@ -10,52 +10,65 @@ $HTTP_STR=$_GET;
 //var_dump($HTTP_STR);
 $num_array=0;
 for ($i = 1; $i <= 10; $i++) {
-$stringa="select".$i;
-switch ($stringa) {
-case 'select1':
-$attr_name="language";
-break;
-case 'select2':
-$attr_name="category";
-break;
-case 'select3':
-$attr_name="d_req_skill";
-break;
-case 'select4':
-$attr_name="d_acq_skill";
-break;
-case 'select5':
-$attr_name="resourcetype";
-break;
-case 'select6':
-$attr_name="difficulty";
-break;
-case 'select7':
-$attr_name="min_age";
-break;
-case 'select8':
-$attr_name="max_age";
-break;
-case 'select9':
-$attr_name="format";
-break;
-case 'select10':
-$attr_name="time";
-break;
-} 
+	$stringa="select".$i;
+	switch ($stringa) {
+		case 'select1':
+		$attr_name="language";
+		break;
+		case 'select2':
+		$attr_name="category";
+		break;
+		case 'select3':
+		$attr_name="d_req_skill";
+		break;
+		case 'select4':
+		$attr_name="d_acq_skill";
+		break;
+		case 'select5':
+		$attr_name="resourcetype";
+		break;
+		case 'select6':
+		$attr_name="difficulty";
+		break;
+		case 'select7':
+		$attr_name="min_age";
+		break;
+		case 'select8':
+		$attr_name="max_age";
+		break;
+		case 'select9':
+		$attr_name="format";
+		break;
+		case 'select10':
+		$attr_name="time";
+		break;
+	} 
 
 
 
-$key_value=$HTTP_STR["key_value"];
-$key_value=clean_phrase_input($key_value);
-$string_key=" and x.id in (SELECT id_section from full_text_section where  MATCH (name,summary,keywords) AGAINST('".$key_value."' IN BOOLEAN MODE));";
+	
+	
+	//$key_value=clean_phrase_input($key_value);
+	//old
+	//$string_key=" and x.id in (SELECT id_section from full_text_section where  MATCH (name,summary,keywords) AGAINST('".$key_value."' IN BOOLEAN MODE));";
+	
+	//vado a prendere le keyword
+	$key_value=$HTTP_STR["key_value"];
+	if ($key_value != ''){
+		$key_value = "'".$key_value."'";
+		//aggiungo virgolette per la query tra le virgole ce separano le key
+		$key_value=str_replace(",", "','", $key_value);
+		//rimuovo spazi
+		$key_value = preg_replace("/\'\s+/", "'", $key_value);
+		$string_key=" and x.id in (SELECT Id_course_sections from mdl_metadata where  mdl_metadata.Property = 'keywords' and mdl_metadata.Value IN(".$key_value."));";
+	}
 	
 
-if ($HTTP_STR[$stringa]!='') {
- $str_metadata=$attr_name."=".$HTTP_STR[$stringa].",-";
- $str_metadata_full=$str_metadata_full.$str_metadata;
- $num_array=$num_array+1;
-}
+	if ($HTTP_STR[$stringa]!='') {
+	 $str_metadata=$attr_name."=".$HTTP_STR[$stringa].",-";
+	 $str_metadata_full=$str_metadata_full.$str_metadata;
+	 $num_array=$num_array+1;
+	}
 
 }
 
@@ -64,32 +77,39 @@ $str_metadata_full=str_replace(",-", "!", $str_metadata_full);
 $stringa_where='';
 
 for ($i = 1; $i <= $num_array; $i++) {
-$nome_array=substr($str_metadata_full, 0, strpos($str_metadata_full, '!'));
-if ($nome_array=='') {$nome_array=$str_metadata_full;}
-$str_metadata_full=substr($str_metadata_full, strlen($nome_array)+1, strlen($str_metadata_full) );
+	$nome_array=substr($str_metadata_full, 0, strpos($str_metadata_full, '!'));
+	if ($nome_array=='') {$nome_array=$str_metadata_full;}
+	$str_metadata_full=substr($str_metadata_full, strlen($nome_array)+1, strlen($str_metadata_full) );
 
-$nome_array_key=substr($nome_array, 0, strpos($nome_array, '='));
-$nome_array=substr($nome_array, strlen($nome_array_key)+1, strlen($nome_array) );
-$array_ex= explode(",", $nome_array);
-$nome_array = "";
-for($j = 0; $j < count($array_ex); $j++)
-	$nome_array = $nome_array."'".$array_ex[$j]."',";
+	$nome_array_key=substr($nome_array, 0, strpos($nome_array, '='));
+	$nome_array=substr($nome_array, strlen($nome_array_key)+1, strlen($nome_array) );
+	$array_ex= explode(",", $nome_array);
+	$nome_array = "";
+	for($j = 0; $j < count($array_ex); $j++)
+		$nome_array = $nome_array."'".$array_ex[$j]."',";
 
-$nome_array = substr($nome_array, 0, -1);
-$nome_array = str_replace("l'", "l\\'", $nome_array);	
+	$nome_array = substr($nome_array, 0, -1);
+	$nome_array = str_replace("l'", "l\\'", $nome_array);	
 
-//x.id = z.Id_course_sections and
-//VERSIONE VECCHIA ---> $stringa_where=$stringa_where."and (exists (select * from mdl_metadata z where  (z.Property='".$nome_array_key."' and z.Value IN (".$nome_array.")))) ";
-$stringa_where=$stringa_where."AND x.id IN (SELECT m.Id_course_sections FROM mdl_metadata m WHERE  m.Property='".$nome_array_key."' and m.Value IN (".$nome_array."))";
+	//x.id = z.Id_course_sections and
+	//VERSIONE VECCHIA ---> $stringa_where=$stringa_where."and (exists (select * from mdl_metadata z where  (z.Property='".$nome_array_key."' and z.Value IN (".$nome_array.")))) ";
+	//se la chiave di ricerca e` una categoria, vado a cercarla anche a livello di corso non solo modulo che potrebbe averla persa
+	if($nome_array_key == "category")
+		$stringa_where=$stringa_where."AND 
+										(x.id IN (SELECT m.Id_course_sections FROM mdl_metadata m WHERE  m.Property='".$nome_array_key."' and m.Value IN (".$nome_array."))
+											OR x.course IN (SELECT m.Id_course FROM mdl_metadata m WHERE  m.Property='".$nome_array_key."' and m.Value IN (".$nome_array."))
+										)";
+	else
+		$stringa_where=$stringa_where."AND x.id IN (SELECT m.Id_course_sections FROM mdl_metadata m WHERE  m.Property='".$nome_array_key."' and m.Value IN (".$nome_array."))";
 
 
-//while(list($chiave,$valore)=each($nome_array_value))
-//		{
-//		$stringa_where=$stringa_where."('".$valore."' in (SELECT mdl_metadata.Value from mdl_metadata  where x.id= mdl_metadata.Id_course_sections and mdl_metadata.Property='".$nome_array_key."')) or ";
-	//	}
-		
-	//	$stringa_where=substr($stringa_where, 0, strlen($stringa_where)-4);
-	//	$stringa_where=$stringa_where.") and (";
+	//while(list($chiave,$valore)=each($nome_array_value))
+	//		{
+	//		$stringa_where=$stringa_where."('".$valore."' in (SELECT mdl_metadata.Value from mdl_metadata  where x.id= mdl_metadata.Id_course_sections and mdl_metadata.Property='".$nome_array_key."')) or ";
+		//	}
+			
+		//	$stringa_where=substr($stringa_where, 0, strlen($stringa_where)-4);
+		//	$stringa_where=$stringa_where.") and (";
 }
 
 
@@ -124,7 +144,7 @@ $sql_sections=$sql_sections." and x.id  not in (select id_sec_dest from nett_dup
   
 //$sql_sections=$sql_sections." and x.id (select id_sec_dest from nett_duplicates WHERE flag = 0)";
 
-//if ($key_value!='*') $sql_sections=$sql_sections.$string_key;
+if ($key_value!='*') $sql_sections=$sql_sections.$string_key;
 	
 echo $sql_sections;
 require 'mysql_conn.php';
